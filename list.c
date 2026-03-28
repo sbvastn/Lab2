92380bb2fc7f48067808388225e056b5f089a8fa
Your Name <you@example.com>
2026-03-28 15:45:25 +0000

    new try

--- list.c
+++ list.c
@@ -93,6 +93,13 @@ void * prevList(List * list) {
 // Puede utilizar la función Node* createNode(void * data) la cual crea, incializa y retorna un nodo con el dato correspondiente.
 
 void pushFront(List * list, void * data) {
+    Node* nuevo = createNode(data);
+    void* aux = list->head;
+    list->head = nuevo;
+    nuevo->next = aux;
+    aux->prev = nuevo;
+    list->current = nuevo;
+    return list->current->data;
 }
 
 void pushBack(List * list, void * data) {
